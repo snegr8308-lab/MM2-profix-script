685910743cdf16c67a71bca9d9c4a4e7e943ce3a
SurFerie20 <snegr8308@gmail.com>
2026-07-03 21:32:35 +0300

    Update UI titles and dropdown options in profix.lua

--- profix.lua
+++ profix.lua
@@ -1,27 +1,22 @@
 local _version = "1.6.6"
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/download/" .. _version .. "/main.lua"))() 
 local Window = WindUI:CreateWindow({
-    Title = "Przykład",
-    Icon = "star",
+    Title = "Profix",
+    Icon = "Star",
     Size = UDim2.fromOffset(500, 400), NewElements = true,
-    Theme = "Amber",
+    Theme = "Red",
     
-    Accent = Color3.fromHex("#b45309"),
-    Button = Color3.fromHex("#f59e0b"),
-    Slider = Color3.fromHex("#d97706"),
-    Color = Color3.fromHex("#fbbf24"),
-    Toggle = Color3.fromHex("d97706"),
 })
 
 local Tab = Window:Tab({Title = "Test", Icon = "settings", ShowTabTitle = true})
 local Section = Tab:Section({Title = "Dropdown Test", Box = true, BoxBorder = true, Opened = true})
 
 local MyDropdown = Section:Dropdown({
-    Title = "Wybierz opcję",
-    Values = {"Opcja 1", "Opcja 2", "Opcja 3"},
+    Title = "options",
+    Values = {"1", "2", "3"},
     Locked = true,
-    LockedTitle = "Zablokowane",
+    LockedTitle = "Locked",
     Callback = function(v)
-        print("Wybrano:", v)
+        print("Choosen:", v)
     end
 })
