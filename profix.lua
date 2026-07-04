9e094b7821facb3dfb622d2d3a5dc64b1dcc2b51
SurFerie20 <snegr8308@gmail.com>
2026-07-04 09:54:00 +0300

    Enhance Profix UI with new features and settings
    Updated the Profix UI with new properties and options.

--- profix.lua
+++ profix.lua
@@ -1,22 +1,62 @@
 local _version = "1.6.6"
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/download/" .. _version .. "/main.lua"))() 
 local Window = WindUI:CreateWindow({
-    Title = "Profix",
-    Icon = "Star",
-    Size = UDim2.fromOffset(500, 400), NewElements = true,
-    Theme = "Red",
+BackgroundVideo = "https://raw.githubusercontent.com/snegr8308-lab/Background_UI_summer/refs/heads/main/BackgroundSummer.mp4",
+    Title = "Profix Hub",
+    Icon = "shield", -- lucide icon
+    Author = "by enormus",
+    Folder = "ProfixHub",
     
-})
-
-local Tab = Window:Tab({Title = "Test", Icon = "settings", ShowTabTitle = true})
-local Section = Tab:Section({Title = "Dropdown Test", Box = true, BoxBorder = true, Opened = true})
-
-local MyDropdown = Section:Dropdown({
-    Title = "options",
-    Values = {"1", "2", "3"},
-    Locked = true,
-    LockedTitle = "Locked",
-    Callback = function(v)
-        print("Choosen:", v)
-    end
+    -- ↓ This all is Optional. You can remove it.
+    Size = UDim2.fromOffset(580, 460),
+    MinSize = Vector2.new(560, 350),
+    MaxSize = Vector2.new(850, 560),
+    ToggleKey = Enum.KeyCode.LeftShift,
+    Transparent = true,
+    Theme = "Dark",
+    Resizable = true,
+    SideBarWidth = 200,
+    BackgroundImageTransparency = 0.42,
+    HideSearchBar = true,
+    ScrollBarEnabled = false,
+    -- ↓ Optional. You can remove it.
+    --[[ You can set 'rbxassetid://' or video to Background.
+        'rbxassetid://':
+            Background = "rbxassetid://", -- rbxassetid
+        Video:
+            Background = "video:YOUR-RAW-LINK-TO-VIDEO.webm", -- video 
+    --]]
+    
+    -- ↓ Optional. You can remove it.
+    User = {
+        Enabled = true,
+        Anonymous = true,
+        Callback = function()
+            print("clicked")
+        end,
+    },
+    
+    --       remove this all, 
+    -- !  ↓  if you DON'T need the key system
+    KeySystem = { 
+        -- ↓ Optional. You can remove it.
+        Key = { "1234", "5678" },
+        
+        Note = "Example Key System.",
+        
+        -- ↓ Optional. You can remove it.
+        Thumbnail = {
+            Image = "rbxassetid://",
+            Title = "Thumbnail",
+        },
+        
+        -- ↓ Optional. You can remove it.
+        URL = "YOUR LINK TO GET KEY (Discord, Linkvertise, Pastebin, etc.)",
+        
+        -- ↓ Optional. You can remove it.
+        SaveKey = false, -- automatically save and load the key.
+        
+        -- ↓ Optional. You can remove it.
+        -- API = {} ← Services. Read about it below ↓
+    },
 })
