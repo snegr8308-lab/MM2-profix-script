2095cf1503a3dbe770769b4efc7c1343b4e959aa
SurFerie20 <snegr8308@gmail.com>
2026-07-04 11:20:10 +0300

    Enhance Profix Hub UI with new sections and tabs
    Updated the Profix Hub UI by modifying the size and adding new sections and tabs.

--- profix.lua
+++ profix.lua
@@ -1,42 +1,43 @@
 local _version = "1.6.6"
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/download/" .. _version .. "/main.lua"))() 
+
 local Window = WindUI:CreateWindow({
     Background = "video:https://raw.githubusercontent.com/snegr8308-lab/Backgrounds-Themes/main/Summer_Background.webm",
     Title = "Profix Hub",
-    Icon = "shield", -- lucide icon
+    Icon = "shield",
     Author = "by enormus",
     Folder = "ProfixHub",
-    
-    -- ↓ This all is Optional. You can remove it.
-    Size = UDim2.fromOffset(580, 460),
-    MinSize = Vector2.new(560, 350),
-    MaxSize = Vector2.new(850, 560),
-    ToggleKey = Enum.KeyCode.LeftShift,
+    Size = UDim2.fromOffset(580, 464),
     Transparent = true,
     Theme = "Amber",
-    Resizable = true,
-    SideBarWidth = 200,
-    BackgroundImageTransparency = 0.32,
-    HideSearchBar = true,
-    ScrollBarEnabled = false,
-    -- ↓ Optional. You can remove it.
-    --[[ You can set 'rbxassetid://' or video to Background.
-        'rbxassetid://':
-            Background = "rbxassetid://", -- rbxassetid
-        Video:
-            Background = "video:YOUR-RAW-LINK-TO-VIDEO.webm", -- video 
-    --]]
-    
-    -- ↓ Optional. You can remove it.
-    User = {
-        Enabled = true,
-        Anonymous = true,
-        Callback = function()
-            print("clicked")
-        end,
-    },
-    
-    --       remove this all, 
-    -- !  ↓  if you DON'T need the key system
-    },
+})
+
+local MainSection = Window:Section({
+    Title = "Main",
+    Icon = "home",
+    Opened = true,
+})
+
+-- Добавляем табы внутрь секции
+local PlayerTab = MainSection:Tab({ 
+    Title = "Player" 
+})
+local SherifTab = MainSection:Tab({ 
+    Title = "Sherif" 
+})
+local MurderTab = MainSection:Tab({ 
+    Title = "Murder" 
+})
+local EcpTab = MainSection:Tab({ 
+    Title = "Ecp" 
+})
+
+local MiscSection = Window:Section({
+    Title = "Misc",
+    Icon = "settings",
+    Opened = true,
+})
+
+local TrollTab = MiscSection:Tab({ 
+    Title = "Troll",
 })
