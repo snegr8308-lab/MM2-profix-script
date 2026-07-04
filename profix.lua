e517d37094619a41803e83152ac016826d9515c0
SurFerie20 <snegr8308@gmail.com>
2026-07-04 20:04:19 +0300

    Update theme and window properties in profix.lua

--- profix.lua
+++ profix.lua
@@ -1,24 +1,33 @@
 local _version = "1.6.6"
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/download/" .. _version .. "/main.lua"))() 
 
+WindUI:AddTheme({
+    Name = "SubAmber",
+    Text = Color3.fromHex("#FFFFFF"),
+    Icon = Color3.fromHex("#f59e0b"),
+})
+
 local Window = WindUI:CreateWindow({
     Background = "video:https://raw.githubusercontent.com/snegr8308-lab/Backgrounds-Themes/main/Orange_video_bg.webm",
+    BackgroundTransparency = 0.67,
     Title = "Profix Hub",
     Icon = "shield",
     Author = "by enormus",
     Folder = "ProfixHub",
-    Size = UDim2.fromOffset(580, 464),
+    Size = UDim2.fromOffset(580, 460),
     Transparent = true,
-    Theme = "Amber",
+    Theme = "SubAmber",
+    User = {
+        Enabled = true,
+        Anonymous = false,
+    },
 })
 
-local MainSection = Window:Section({
-    Title = "Main",
-    Icon = "home",
-    Opened = true,
+local MainSection = Window:Section({ 
+    Title = "Main", 
+    Icon = "home", 
+    Opened = true 
 })
-
--- Добавляем табы внутрь секции
 local PlayerTab = MainSection:Tab({ 
     Title = "Player" 
 })
@@ -32,12 +41,11 @@ local EcpTab = MainSection:Tab({
     Title = "Ecp" 
 })
 
-local MiscSection = Window:Section({
-    Title = "Misc",
-    Icon = "settings",
-    Opened = true,
+local MiscSection = Window:Section({ 
+    Title = "Misc", 
+    Icon = "settings", 
+    Opened = false 
 })
-
 local TrollTab = MiscSection:Tab({ 
-    Title = "Troll",
+    Title = "Troll" 
 })
